b1b1cbfcad1a18efd21e59be359356b9cebfd64e
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-08-05 10:33:24 +0530

    Create required_software.sh

---
+++ required_software.sh
@@ -0,0 +1,6 @@
+#run this script to install the required software
+#Author : Eshwar RA
+#mail   : eshwarra5@gmail.com
+
+sudo apt update
+sudo apt install 'tor' 'apache2' 'python3' 'nodejs' 'build-essential' 'manpages-dev'
