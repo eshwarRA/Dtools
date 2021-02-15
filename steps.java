3d4f584bdf9032cfb694d7e690a58692f45281cb
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2021-02-15 10:26:40 +0530

    Add files via upload

---
+++ steps.java
@@ -0,0 +1,21 @@
+//Author: Eshwar RA
+//mail  : eshwarra5@gmail.com
+//Description:
+//This tool can be used in linux servers if you dont have any knowledge of the
+//linux command line. Please comment/contribute for more tools.
+class steps{
+      Scanner sc = new Scanner(System.in);
+      public void apt(){
+            System.out.println("1] installing packages/software");
+            int opApt = sc.nextInt();
+            System.out.println();
+            if(opApt == 1){
+                  System.out.println("Learn to install packages on debian using apt =>");
+                  System.out.println();
+                  System.out.println();
+            }
+      public static void main(String[]args){
+            System.out.println("Welcome to the system toolkit");
+            System.out.println();
+            System.out.println("using apt for installing and removing packages/software");
+
